3e0668cdb9157624f5a7207fd61f6b8dd9dc16e0
memarino92 <memarino92@gmail.com>
2022-01-22 15:36:42 -0500

    Added requireAuth() calls to expense service

--- api/src/services/expenses/expenses.ts
+++ api/src/services/expenses/expenses.ts
@@ -1,15 +1,18 @@
 import type { Prisma } from '@prisma/client'
 import type { ResolverArgs } from '@redwoodjs/graphql-server'
+import { requireAuth } from 'src/lib/auth'
 
 import { db } from 'src/lib/db'
 
 export const expenses = ({ expenseListId }) => {
+  requireAuth({ roles: [] })
   return db.expense.findMany({
     where: { expenseListId },
   })
 }
 
 export const expense = ({ id }: Prisma.ExpenseWhereUniqueInput) => {
+  requireAuth({ roles: [] })
   return db.expense.findUnique({
     where: { id },
   })
