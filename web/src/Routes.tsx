e2d044a0b95c83fb2f96dbb322fac18b12bccffe
memarino92 <memarino92@gmail.com>
2022-01-22 13:55:14 -0500

    Generated homepage

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,11 +7,14 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Private } from '@redwoodjs/router'
 
 const Routes = () => {
   return (
     <Router>
+      <Private unauthenticated="login">
+        <Route path="/" page={HomePage} name="home" />
+      </Private>
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,30 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  const { logOut } = useAuth()
+
+  const handleClick = () => {
+    logOut()
+  }
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+      <button type="button" onClick={handleClick}>
+        Log Out
+      </button>
+    </>
+  )
+}
+
+export default HomePage
