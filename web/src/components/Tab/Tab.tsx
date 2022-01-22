55adeea4ad816bbac85a299073ebf8b840b063c9
memarino92 <memarino92@gmail.com>
2022-01-22 16:26:16 -0500

    Built out Tab component and story

--- web/src/components/Tab/Tab.tsx
+++ web/src/components/Tab/Tab.tsx
@@ -1,8 +1,19 @@
-const Tab = () => {
+import ListCard from '../ListCard/ListCard'
+import ResultCard from '../ResultCard/ResultCard'
+
+const Tab = ({ tab }) => {
+  const { expenseLists } = tab
+  const totalOne = expenseLists[0].expenses.reduce((a, b) => a + b.amount, 0)
+  const totalTwo = expenseLists[1].expenses.reduce((a, b) => a + b.amount, 0)
   return (
     <div>
-      <h2>{'Tab'}</h2>
-      <p>{'Find me in ./web/src/components/Tab/Tab.tsx'}</p>
+      <div className="flex w-100 justify-center align-middle">
+        <ListCard expenseList={expenseLists[0]} total={totalOne} />
+        <ListCard expenseList={expenseLists[1]} total={totalTwo} />
+      </div>
+      <div className="flex w-100 justify-center align-middle">
+        <ResultCard totalOne={totalOne} totalTwo={totalTwo} />
+      </div>
     </div>
   )
 }
