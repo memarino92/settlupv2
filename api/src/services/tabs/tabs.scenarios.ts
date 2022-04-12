c0f668d2faa6403f32fe806b8ce36c9cef7d3f84
memarino92 <memarino92@gmail.com>
2022-04-11 22:01:22 -0400

    fix: correct twitter handle in README

--- api/src/services/tabs/tabs.scenarios.ts
+++ api/src/services/tabs/tabs.scenarios.ts
@@ -1,7 +1,26 @@
 import type { Prisma } from '@prisma/client'
 
 export const standard = defineScenario<Prisma.TabCreateArgs>({
-  tab: { one: { data: {} }, two: { data: {} } },
+  tab: {
+    one: {
+      data: {
+        name: 'name',
+        users: {
+          connect: {
+            id: 1,
+          },
+        },
+        expenseLists: {
+          createMany: {
+            data: [
+              { userId: 1, name: 'testUserOne' },
+              { userId: 1, name: 'testUserTwo' },
+            ],
+          },
+        },
+      },
+    },
+  },
 })
 
 export type StandardScenario = typeof standard
